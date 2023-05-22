4418fbc6b961b19054627b837a31caaa89b0f27b
msmarjorine <marjorine13@gmail.com>
2023-05-22 16:26:11 +0300

    Lifecycle methods

--- src/test/java/junit5tests/FirstTestClass.java
+++ src/test/java/junit5tests/FirstTestClass.java
@@ -1,9 +1,19 @@
 package junit5tests;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FirstTestClass {
+
+    @BeforeAll
+    void printBeforeAll(){
+        System.out.println("This method set ups environment before all tests.");
+    }
+
+    @BeforeEach
+    void printBeforeEach(){
+        System.out.println("--before each method");
+    }
 
     @Test
     void firstMethod(){
@@ -14,5 +24,15 @@ public class FirstTestClass {
     @DisplayName("US1234 - TC12 - this method is the second one")
     void secondMethod(){
         System.out.println("This is the second test method.");
+    }
+
+    @AfterEach
+    void printAfterEach(){
+        System.out.println("--after each method");
+    }
+
+    @AfterAll
+    void printAfterAll(){
+        System.out.println("This method cleans environment after all tests.");
     }
 }
