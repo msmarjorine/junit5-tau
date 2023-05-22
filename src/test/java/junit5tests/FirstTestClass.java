9c2c3ece80a7391f92daeb250c26ced259e9168d
msmarjorine <marjorine13@gmail.com>
2023-05-22 16:13:43 +0300

    The project created to learn JUnit5

---
+++ src/test/java/junit5tests/FirstTestClass.java
@@ -0,0 +1,18 @@
+package junit5tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FirstTestClass {
+
+    @Test
+    void firstMethod(){
+        System.out.println("This is the first test method.");
+    }
+
+    @Test
+    @DisplayName("US1234 - TC12 - this method is the second one")
+    void secondMethod(){
+        System.out.println("This is the second test method.");
+    }
+}
