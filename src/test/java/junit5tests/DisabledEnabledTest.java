05ae18a9e3e380c736e0cb357cb3d45df2e42cac
msmarjorine <marjorine13@gmail.com>
2023-05-23 14:40:46 +0300

    Add disabled tests'

---
+++ src/test/java/junit5tests/DisabledEnabledTest.java
@@ -0,0 +1,47 @@
+package junit5tests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class DisabledEnabledTest {
+
+
+    @Test
+    @Disabled(value="Disabled for demo of @Disabled")
+    void firstTest(){
+        System.out.println("This is the first test method");
+    }
+
+    @Test
+    @DisabledOnOs(value= OS.WINDOWS, disabledReason="This is disabled for demo of @DisabledOnOs")
+    //@EnabledOnOs(value=(OS.MAC))
+    void secondTest(){
+        System.out.println("This is the second test");
+    }
+
+    //These properties don't work for me for some reason
+    @Test
+    @DisabledIfSystemProperty(named="env", matches="staging", disabledReason = "Disabled for demo of @DisabledIfSystemProperty")
+    void thirdTest(){
+        System.out.println("This is the third test");
+    }
+
+    @Test
+    @DisabledIf(value="provider", disabledReason = "Disabled for demo of @DisabledIf")
+    void fourthTest(){
+        System.out.println("This is the fourth test");
+    }
+
+    @Test
+    void fifthTest(){
+        System.out.println("This is the fifth test");
+    }
+
+    boolean provider(){
+        return LocalDateTime.now().getDayOfWeek().equals(DayOfWeek.TUESDAY);
+    }
+}
