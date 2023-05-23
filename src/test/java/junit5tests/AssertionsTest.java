fd5a5fb88816316161523dbe23430a51265cdd41
msmarjorine <marjorine13@gmail.com>
2023-05-23 17:38:25 +0300

    Add Hamcrest dependency

--- src/test/java/junit5tests/AssertionsTest.java
+++ src/test/java/junit5tests/AssertionsTest.java
@@ -1,11 +1,18 @@
 package junit5tests;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AssertionsTest {
@@ -17,8 +24,8 @@ public class AssertionsTest {
 
     @Test
     void asserEqualsListTest(){
-        List<String> expectedValues = Arrays.asList("firstString", "secondString", "thirdString");
-        List<String> actualValues = Arrays.asList("firstString", "secondString", "thirdString");
+        List<String> expectedValues = asList("firstString", "secondString", "thirdString");
+        List<String> actualValues = asList("firstString", "secondString", "thirdString");
         assertEquals(expectedValues, actualValues);
     }
 
@@ -47,5 +54,38 @@ public class AssertionsTest {
                 () -> assertThrows(NullPointerException.class, null),
                 () -> assertTrue(false, "This boolean condition did not evaluate to true")
         );
+    }
+
+    @Test
+    void assertForMapTest(){
+        Map<String, Integer> theMap = new HashMap<>();
+        theMap.put("firstKey", 1);
+        theMap.put("secondKey", 2);
+        theMap.put("thirdKey", 3);
+
+        assertThat(theMap, Matchers.hasKey("secondKey"));
+        assertThat(theMap, Matchers.hasValue(3));
+    }
+
+    @Test
+    void assertForListTest(){
+        List<String> theList = asList("firstString", "secondString", "thirdString");
+
+        assertThat(theList, hasItem("thirdString"));
+    }
+
+    @Test //doesn't work ??
+    void assertForAnyOfTest(){
+        List<String> theList = asList("firstString", "secondString", "thirdString");
+
+        assertThat(theList, anyOf(hasItem("thirdString"),
+                hasItem("fourthString")));
+    }
+
+    @Test //doesn't work ??
+    void assertForContainsAnyOrder(){
+        List<String> theList = asList("firstString", "secondString", "thirdString");
+
+        assertThat(theList, containsInAnyOrder("firstString", "secondString", "thirdString"));
     }
 }
